0241349a5aab15997e0e4c891ca79b9b209ae63b
espatulo <espatuladeoro@gmail.com>
2018-11-24 21:14:45 +0100

    first commit

---
+++ R/rand_values.R
@@ -0,0 +1,14 @@
+#' A Cat Function
+#'
+#' This function allows you to get n random numbers
+#' @param n Number of random numbers
+#' @keywords random numbers
+#' @export
+#' @examples
+#' rand_values()
+
+
+rand_values<-function(n=50){
+  runif(n)
+}
+
